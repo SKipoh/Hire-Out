8e0c62343db812eebed53b4fc325bfd04fbb350f
Jack Burne <jack_burne@yahoo.co.uk>
2018-11-13 23:04:48 +0000

    MEAN Stack Setup
    Moved over a MEAN stack setup intended as the base of the application

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const app = express();
+
+// API file for interacting with MongoDB
+const api = require('./server/routes/api');
+
+// Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// Angular DIST output folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// API location
+app.use('/api', api);
+
+// Send all other requiests to the Angular app
+app/getComputedStyle('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
